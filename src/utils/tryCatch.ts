02203631749ffbe4544ffc27ce60e4b05e050834
Ofek Tal <ofeki884@gmail.com>
2026-02-20 01:18:49 +0200

    prettiffied tryCatch util
    using my types and replacing any with unkown

--- src/utils/tryCatch.ts
+++ src/utils/tryCatch.ts
@@ -1,21 +1,23 @@
+import type { Func } from "../types";
+
 type Success<T> = readonly [null, T];
 type Failure<E> = readonly [E, null];
 type Result<E, T> = Success<T> | Failure<E>;
-type ResultAsync<E, T> = Promise<Result<E, T>>;
 
-export function tryCatch<E = Error, T = Promise<any>>(
+export function tryCatch<E = Error, T = unknown>(
+  operation: Func<T>,
+): Result<E, T>;
+export function tryCatch<E = Error, T = Promise<unknown>>(
   operation: T,
-): ResultAsync<E, T>;
-export function tryCatch<E = Error, T = any>(operation: () => T): Result<E, T>;
-export function tryCatch<E = Error, T = any>(
-  operation: Promise<T> | (() => T),
-): Result<E, T> | ResultAsync<E, T> {
+): Promise<Result<E, T>>;
+export function tryCatch<E = Error, T = unknown>(
+  operation: Promise<T> | Func<T>,
+): Result<E, T> | Promise<Result<E, T>> {
   if (operation instanceof Promise) {
     return operation
       .then((value: T) => [null, value] as const)
       .catch((error: E) => [error, null] as const);
   }
-
   try {
     const data = operation();
     return [null, data];
